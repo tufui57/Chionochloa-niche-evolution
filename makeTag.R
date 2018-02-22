19b60c9bf826a0f2ff06298dc43874f8a93726b3
DESKTOP-3U6270B\nomur <nommi038@student.otago.ac.nz>
2018-02-23 08:51:56 +1300

    Added argument for separation

--- makeTag.R
+++ makeTag.R
@@ -32,4 +32,36 @@ makeTag <- function(data, # vector of full species names
 
 
 
+makeTag_separate <- function(data, # vector of full species names 
+                             genus_name, # genus name should be got rid of
+                             separate # syntax which separates genus and species names e.g. "_" between "Acaena_agnipila"
+){
+  ### Import species name codes
+  spname <- grepl(genus_name, data) %>% data[.]
+  codes <- gsub(paste(genus_name, separate, sep=""), "", spname) %>% 
+    gsub(paste("subsp.", separate, sep=""), "", .) %>% 
+    gsub(paste("var.", separate, sep=""), "", .)
+  
+  spname <- (codes %>% substring(., 1, last = 3) %>% mutate(as_tibble(spname), tag = .))
+  colnames(spname)[1] <- "X"
+  subsp <- codes %>% 
+    strsplit(., separate) %>% 
+    lapply(., function(x){
+      ifelse(is.na(x[2]), "", x[2])
+    }) %>% 
+    substring(., 1, last = 3)
+  
+  spname <- lapply(1:length(subsp), function(i){
+    paste(spname[i,"tag"], subsp[i], sep = "_")
+  }
+  ) %>% unlist %>% 
+    gsub(paste(separate, "$", sep=""), "", .) %>% 
+    mutate(spname, tag = .) 
+  
+  return(spname)
+  
+}
+
+
+
 
