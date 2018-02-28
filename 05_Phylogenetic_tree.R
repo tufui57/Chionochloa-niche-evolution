499dc0f6b144ce3c26e7fedf70674ca600957d9a
DESKTOP-3U6270B\nomur <nommi038@student.otago.ac.nz>
2018-03-01 09:32:20 +1300

    Phylogeny tree changed extracted one which excluded outgroups.

--- 05_Phylogenetic_tree.R
+++ 05_Phylogenetic_tree.R
@@ -1,25 +1,22 @@
 ########################################################################################
 ### Phylogeny tree drawing
 ########################################################################################
+genus_name = "Chionochloa"
 
-library(adegenet)
-library(geiger)
-library(phytools)
-
-# Import tree data
-chion <- read.nexus("Y:\\Niche change of lineages\\Niche evolution of open habitat species in islands\\Phylogenetic data and trees\\Chionochloa_genetic_data\\Chiono_summary.trees")
+source(".//Chionochloa niche evolution//09_DataPreparation.R")
 
 ########################################################################################
 ### Plot edge length on trees
 ########################################################################################
 
-png("Y:\\chion phylogeny tree.png", width = 1800, height = 1250)
-plotTree(chion, adj = 0, label.offset = 0.75, no.margin = TRUE, cex = 1.5)
-# edgelabels(round(chion$edge.length, 5),
+tree$tip.label <- clean_speciesname(tree$tip.label)
+
+png(paste("Y:\\", genus_name , "_phylogeny_tree.png", sep = ""), width = 1800, height = 1250)
+plotTree(tree, adj = 0, label.offset = 0.75, no.margin = T, cex = 1.5)
+# edgelabels(round(extracted.tree$edge.length, 5),
 #            adj = 2,
 #            cex = 1.5
 #            )
-nodelabels(cex = 1.5, adj = 2)
-tiplabels(cex = 1.5,adj = 1)
+nodelabels(cex = 1.5, adj = 1)
+tiplabels(cex = 2,adj = 1)
 dev.off()
-
